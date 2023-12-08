eca28145d8f9c9280eac38ad341521fcf7c4489d
Valalol <valentin.esnault92@gmail.com>
2023-12-08 14:38:24 +0100

    day 8 v2

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,3 +1,8 @@
+def gcd(a, b):
+    while b>0:
+        a, b = b, a%b
+    return a
+
 def main(data, phase=1):
     lines = data.split("\n")
     input_commands = list(lines.pop(0))
@@ -9,17 +14,43 @@ def main(data, phase=1):
     print(input_commands)
     print(nodes)
     
-    i = 0
-    node = 'AAA'
-    while node != 'ZZZ':
-        move = input_commands[i%len(input_commands)]
-        if move == 'L':
-            node = nodes[node][0]
-        else:
-            node = nodes[node][1]
-        i += 1
+    if phase == 1:
+        i = 0
+        node = 'AAA'
+        while node != 'ZZZ':
+            move = input_commands[i%len(input_commands)]
+            if move == 'L':
+                node = nodes[node][0]
+            else:
+                node = nodes[node][1]
+            i += 1
+        return i
     
-    return i
+    
+    if phase == 2:
+        node_list = []
+        for node in nodes:
+            if node[-1] == "A":
+                node_list.append(node)
+        
+        turns_list = []
+        
+        for node in node_list:
+            i = 0
+            while node[-1] != 'Z':
+                move = input_commands[i%len(input_commands)]
+                if move == 'L':
+                    node = nodes[node][0]
+                else:
+                    node = nodes[node][1]
+                i += 1
+            turns_list.append(i)
+        
+        ppcm = turns_list[0]
+        for i in range(1, len(turns_list)):
+            ppcm = int(ppcm*turns_list[i] / gcd(ppcm, turns_list[i]))
+        
+        return ppcm
 
 
 
@@ -36,5 +67,5 @@ ZZZ = (ZZZ, ZZZ)"""
     
     
     
-    result = main(data, 1)
+    result = main(data, 2)
     print(result)
