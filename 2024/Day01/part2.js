17e5ef49457532c1ac8b85c2171b449a08071769
Valentin Esnault <valentin.esnault@easyvirt.com>
2024-12-01 16:30:58 +0100

    added timing to part2

--- 2024/Day01/part2.js
+++ 2024/Day01/part2.js
@@ -1,8 +1,10 @@
 const fs = require('node:fs');
 
 data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
-data = data.split('\r\n').map(line => line.split('   ').map(value => parseInt(value)))
 
+const startTime = performance.now()
+
+data = data.split('\r\n').map(line => line.split('   ').map(value => parseInt(value)))
 data = data[0].map((_, col_index) => data.map(ligne => ligne[col_index]))
 
 const counter = {};
@@ -14,4 +16,7 @@ similarity_scores = data[0].map(value => counter[value] ? value * counter[value]
 
 sum = similarity_scores.reduce((partialSum, a) => partialSum + a, 0)
 
+const endTime = performance.now()
+
 console.log(JSON.stringify(sum))
+console.log(`Code took ${endTime - startTime} milliseconds`)
