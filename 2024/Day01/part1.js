ab88d1df749c79ba2d6de3cc11b4b4f957a994b0
Valentin Esnault <valentin.esnault@easyvirt.com>
2024-12-01 16:29:05 +0100

    change lines order

--- 2024/Day01/part1.js
+++ 2024/Day01/part1.js
@@ -1,18 +1,16 @@
-const startTime = performance.now()
-
 const fs = require('node:fs');
 
 data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
+
+const startTime = performance.now()
+
 data = data.split('\r\n').map(line => line.split('   ').map(value => parseInt(value)))
-
 data = data[0].map((_, col_index) => data.map(ligne => ligne[col_index]))
-
 data.map(list => list.sort((a,b) => a - b))
-
 diffs = data[0].map((_,i) => Math.abs(data[0][i] - data[1][i]));
-sum = diffs.reduce((partialSum, a) => partialSum + a, 0)
-
-console.log(JSON.stringify(sum))
+result = diffs.reduce((partialSum, a) => partialSum + a, 0)
 
 const endTime = performance.now()
+
+console.log(JSON.stringify(result))
 console.log(`Code took ${endTime - startTime} milliseconds`)
