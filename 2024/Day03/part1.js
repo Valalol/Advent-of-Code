399cc6b20b5b8546ec9a026564fea320b3b7918e
Valentin Esnault <valentin.esnault@easyvirt.com>
2024-12-03 20:35:48 +0100

    change for matchAll with capture groups in regex

--- 2024/Day03/part1.js
+++ 2024/Day03/part1.js
@@ -8,6 +8,7 @@ const startTime = performance.now()
 // (?=<string>) : lookahead
 // /(?<=mul\()\d+,\d+(?=\))/g
 // result = data.match(/(?<=mul\()\d+,\d+(?=\))/g).map(match => match.split(',').map(value => parseInt(value))).map(mult => mult[0] * mult[1]).reduce((partialSum, a) => partialSum + a, 0)
+
 result = data.matchAll(/mul\((\d+),(\d+)\)/g).map(mult => parseInt(mult[1]) * parseInt(mult[2])).reduce((partialSum, a) => partialSum + a, 0)
 
 const endTime = performance.now()
--- 2024/Day03/part2.js
+++ 2024/Day03/part2.js
@@ -6,10 +6,10 @@ const startTime = performance.now()
 
 // (?<=<string>) : lookbehind
 // (?=<string>) : lookahead
-result = data.match(/(?<=mul\()\d+,\d+(?=\))|don't\(\)|do\(\)/g).reduce((acc, element) => {
-    if (element == "do()") {acc[1] = true; return acc}
-    else if (element == "don't()") {acc[1] = false; return acc}
-    else if (acc[1]) {acc[0] += element.split(',').map(value => parseInt(value)).reduce((partialProduct, a) => partialProduct * a, 1); return acc}
+result = data.matchAll(/mul\((\d+),(\d+)\)|don't\(\)|do\(\)/g).reduce((acc, element) => {
+    if (element[0] == "do()") {acc[1] = true; return acc}
+    else if (element[0] == "don't()") {acc[1] = false; return acc}
+    else if (acc[1]) {acc[0] += parseInt(element[1]) * parseInt(element[2]); return acc}
     else return acc
 }, [0, true])[0]
 
