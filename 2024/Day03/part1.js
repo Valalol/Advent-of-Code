5167d19db9c27d2afe5c5634adff4d757caa781a
Valentin Esnault <valentin.esnault@easyvirt.com>
2024-12-03 17:20:44 +0100

    day03 working

--- 2024/Day03/part1.js
+++ 2024/Day03/part1.js
@@ -4,7 +4,11 @@ data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
 
 const startTime = performance.now()
 
-result = data.match(/mul\(\d*,\d*\)/gm).map(match => match.slice(4,-1).split(',').map(value => parseInt(value))).map(mult => mult[0] * mult[1]).reduce((partialSum, a) => partialSum + a, 0)
+// (?<=<string>) : lookbehind
+// (?=<string>) : lookahead
+// /(?<=mul\()\d+,\d+(?=\))/g
+// result = data.match(/(?<=mul\()\d+,\d+(?=\))/g).map(match => match.split(',').map(value => parseInt(value))).map(mult => mult[0] * mult[1]).reduce((partialSum, a) => partialSum + a, 0)
+result = data.matchAll(/mul\((\d+),(\d+)\)/g).map(mult => parseInt(mult[1]) * parseInt(mult[2])).reduce((partialSum, a) => partialSum + a, 0)
 
 const endTime = performance.now()
 
--- 2024/Day03/part2.js
+++ 2024/Day03/part2.js
@@ -4,7 +4,14 @@ data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
 
 const startTime = performance.now()
 
-result = data.match(/(?<!don't\(\).*?)(?:do\(\).*?)?mul\(\d+,\d+\)/g)
+// (?<=<string>) : lookbehind
+// (?=<string>) : lookahead
+result = data.match(/(?<=mul\()\d+,\d+(?=\))|don't\(\)|do\(\)/g).reduce((acc, element) => {
+    if (element == "do()") {acc[1] = true; return acc}
+    else if (element == "don't()") {acc[1] = false; return acc}
+    else if (acc[1]) {acc[0] += element.split(',').map(value => parseInt(value)).reduce((partialProduct, a) => partialProduct * a, 1); return acc}
+    else return acc
+}, [0, true])[0]
 
 const endTime = performance.now()
 
