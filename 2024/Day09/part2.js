b17f787cdf4e87abfe3e48d1be77a9a004d3c60f
Valentin Esnault <valentin.esnault@easyvirt.com>
2024-12-09 15:57:55 +0100

    Day09 part 2 working

--- 2024/Day09/part2.js
+++ 2024/Day09/part2.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs');
+const { start } = require('node:repl');
 
 
 let data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
@@ -13,10 +14,131 @@ data.map(Number).forEach((value, index) => {
     expanded_data.push(...values)
 })
 
-let start_index = 0
+
+
+
 let end_index = expanded_data.length - 1
 
+while (end_index > 0) {
+    let length = 1
+    while (expanded_data[end_index - length] == expanded_data[end_index]) {
+        length++
+    }
+    // console.log(length)
 
+    let start_index = 0
+    let moved = false
+    while (!moved && (start_index < end_index)) {
+        // console.log(start_index, end_index, JSON.stringify(expanded_data))
+        while (expanded_data[start_index] != null) {
+            start_index++
+        }
+        if (start_index >= end_index) {
+            break
+        }
+
+        let valid = true
+        for (let i = start_index; i < start_index + length; i++) {
+            if (expanded_data[i] != null) {
+                valid = false
+            }
+        }
+
+        if (valid) {
+            for (let i = 0; i < length; i++) {
+                expanded_data[start_index + i] = expanded_data[end_index - i]
+                expanded_data[end_index - i] = null
+            }
+            moved = true
+        }
+        else {
+            start_index++
+        }
+    }
+    end_index -= length
+    while (expanded_data[end_index] == null && end_index > 0) {
+        end_index--
+    }
+}
+
+// while start_index != null : start_index++
+// 00...111...2...333.44.5555.6666.777.888899
+//   ^                                      ^
+// if for i in range(start_index, start_index + length): all case == null -> true
+// 00...111...2...333.44.5555.6666.777.888899
+//    ^
+// then for i in range(0, length)
+// expanded_data[start_index + i] = expanded_data[end_index - i]
+// start_index += length
+
+// --->
+// 0099.111...2...333.44.5555.6666.777.8888..
+//     ^                                  ^
+// length = 4
+// if for i in range(start_index, start_index + length): all case == null -> false
+
+// no space so start_index += length
+// 0099.111...2...333.44.5555.6666.777.8888..
+//         ^                              ^
+// while start_index != null : start_index++
+// 0099.111...2...333.44.5555.6666.777.8888..
+//         ^                              ^
+// if for i in range(start_index, start_index + length): all case == null -> false
+// no space so start_index += length
+// 00...111...2...333.44.5555.6666.777.8888..
+//             ^                          ^
+// while start_index != null : start_index++
+// 00...111...2...333.44.5555.6666.777.8888..
+//             ^                          ^
+// if for i in range(start_index, start_index + length): all case == null -> false
+// no space so start_index += length
+// 00...111...2...333.44.5555.6666.777.8888..
+//                 ^                      ^
+// while start_index != null : start_index++
+// 00...111...2...333.44.5555.6666.777.8888..
+//                   ^                    ^
+// if for i in range(start_index, start_index + length): all case == null -> false
+// no space so start_index += length
+// 00...111...2...333.44.5555.6666.777.8888..
+//                       ^                ^
+// while start_index != null : start_index++
+// 00...111...2...333.44.5555.6666.777.8888..
+//                           ^            ^
+// if for i in range(start_index, start_index + length): all case == null -> false
+// no space so start_index += length
+// 00...111...2...333.44.5555.6666.777.8888..
+//                               ^        ^
+// while start_index != null : start_index++
+// 00...111...2...333.44.5555.6666.777.8888..
+//                                ^       ^
+// if for i in range(start_index, start_index + length): all case == null -> false
+// no space so start_index += length
+// 00...111...2...333.44.5555.6666.777.8888..
+//                                    ^   ^
+// while start_index != null : start_index++
+// 00...111...2...333.44.5555.6666.777.8888..
+//                                    ^   ^
+// if for i in range(start_index, start_index + length): all case == null -> false
+// no space so start_index += length
+// 00...111...2...333.44.5555.6666.777.8888..
+//                                        ^
+// start_index >= end_index -> stop
+
+// start_index = 0
+// end_index - length 
+// while end_index == null: end_index--
+// 00...111...2...333.44.5555.6666.777.8888..
+//                                   ^
+
+// start_index++
+// console.log(JSON.stringify(expanded_data))
+
+let result = 0
+expanded_data.forEach((value, index) => {
+    if (value != null) {
+        result += value * index
+    }
+})
 
 const endTime = performance.now()
 
