05555680789870035694e0f742b84b0d78e3545c
Valalol <valentin.esnault92@gmail.com>
2024-12-10 19:00:19 +0100

    Day10 both parts working

--- 2024/Day09/part2.js
+++ 2024/Day09/part2.js
@@ -1,5 +1,4 @@
 const fs = require('node:fs');
-const { start } = require('node:repl');
 
 
 let data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
@@ -7,8 +6,8 @@ let data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
 const startTime = performance.now()
 
 let expanded_data = []
-data = data.split("")
-data.map(Number).forEach((value, index) => {
+data = data.split("").map(Number)
+data.forEach((value, index) => {
     let fill_value = index%2 == 0 ? Math.floor(index/2) : null
     let values = Array(value).fill(fill_value)
     expanded_data.push(...values)
---
+++ 2024/Day10/part2.js
@@ -0,0 +1,49 @@
+const fs = require('node:fs');
+
+
+let data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
+
+const startTime = performance.now()
+
+data = data.split("\n").map(line => line.split("").map(Number))
+
+
+let height = data.length
+let width = data[0].length
+let trailheads = []
+
+for (let line_index = 0; line_index < height; line_index++) {
+    let line = data[line_index]
+    for (let col_index = 0; col_index < width; col_index++) {
+        element = line[col_index]
+        if (element == 0) {
+            trailheads.push([line_index, col_index])
+        }
+    }
+}
+
+function rec_search_path(old_value, x, y) {
+    // console.log(`Current cell : (${x},${y}) : ${data[x][y]}, Looking for ${old_value + 1}`);
+    if (data[x][y] != old_value + 1 || data[x][y] == NaN) {
+        return []
+    }
+    if (data[x][y] == 9) {
+        // console.log("found", x, y)
+        return [[x, y]]
+    }
+    let return_value = []
+    if (x > 0) return_value.push(...rec_search_path(old_value + 1, x - 1, y))
+    if (x < height - 1) return_value.push(...rec_search_path(old_value + 1, x + 1, y))
+    if (y > 0) return_value.push(...rec_search_path(old_value + 1, x, y - 1))
+    if (y < width - 1) return_value.push(...rec_search_path(old_value + 1, x, y + 1))
+    return return_value
+}
+
+let trail_scores = trailheads.map(trailhead => rec_search_path(-1, trailhead[0], trailhead[1])).map(trail_ends => trail_ends.length)
+
+let result = trail_scores.reduce((partial_sum, value) => partial_sum + value, 0)
+
+const endTime = performance.now()
+
+console.log(JSON.stringify(result))
+console.log(`Code took ${endTime - startTime} milliseconds`)
