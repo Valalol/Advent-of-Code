5a1d63e5454b1224e8b7a5de7c44779717967856
Valalol <valentin.esnault92@gmail.com>
2024-12-18 15:21:22 +0100

    Day17 part 2 bruteforce flop

---
+++ 2024/Day17/part2.js
@@ -0,0 +1,89 @@
+const fs = require('node:fs');
+
+
+let data = fs.readFileSync(__dirname+'/input.txt', "utf-8")
+
+// console.profile()
+const startTime = performance.now()
+
+data = data.split("\n")
+let og_A = -1
+let og_B = Number(data[1].split(": ")[1])
+let og_C = Number(data[2].split(": ")[1])
+let instructions = data[4].split(": ")[1].split(",").map(Number)
+
+
+function combo_operands(operand) {
+    if (operand >= 0 && operand < 4) {
+        return operand
+    } else if (operand == 4) {
+        return A
+    } else if (operand == 5) {
+        return B
+    } else if (operand == 6) {
+        return C
+    } else if (operand == 7) {
+        console.log("Uh wut ??")
+        return -1
+    }
+}
+
+function process_op_code(op_code, operand) {
+    switch (op_code) {
+        case 0:
+            A = Math.floor(A / 2**combo_operands(operand))
+            instruction_pointer += 2
+            break
+        case 1:
+            B ^= operand
+            instruction_pointer += 2
+            break
+        case 2:
+            B = combo_operands(operand)%8
+            instruction_pointer += 2
+            break
+        case 3:
+            if (A != 0) instruction_pointer = operand
+            else instruction_pointer += 2
+            break
+        case 4:
+            B ^= C
+            instruction_pointer += 2
+            break
+        case 5:
+            out.push(combo_operands(operand)%8)
+            instruction_pointer += 2
+            break
+        case 6:
+            B = Math.floor(A / 2**combo_operands(operand))
+            instruction_pointer += 2
+            break
+        case 7:
+            C = Math.floor(A / 2**combo_operands(operand))
+            instruction_pointer += 2
+            break
+    }
+}
+
+out = []
+while (out.join(',') != instructions.join(',')) {
+    out = []
+    og_A += 1
+    if (og_A%100_000 == 0) console.log(og_A.toLocaleString('fr-FR'))
+    A = og_A
+    B = og_B
+    C = og_C
+    instruction_pointer = 0
+    while (instruction_pointer < instructions.length) {
+        process_op_code(instructions[instruction_pointer], instructions[instruction_pointer + 1])
+    }
+}
+
+let result = og_A
+
+
+const endTime = performance.now()
+// console.profileEnd()
+
+console.log(JSON.stringify(result))
+console.log(`Code took ${endTime - startTime} milliseconds`)
